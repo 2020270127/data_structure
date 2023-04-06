3c66e196ab3ef0e6fc05e843d6b1138fcc0f0b24
2020270127 <tottenham2968@gmail.com>
2023-04-06 17:46:29 +0900

    4.6_1

---
+++ DataStructure/struct.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define FALSE 0
+#define TRUE 1
+typedef struct _humanBeing {
+	char* name;
+	int age;
+	float salary;
+	struct _humanBeing* a;
+} humanBeing;
+humanBeing omg, homg;
+
+int humansEqual(humanBeing *person1, humanBeing *person2) {
+	if (strcmp(person1->name, person2->name))
+		return FALSE;
+	if (person1->age != person2->age)
+		return FALSE;
+	if (person1->salary != person2->salary)
+		return FALSE;
+	return TRUE;
+
+}
+int main() {
+	struct personal_info{ //구조체 이름 반드시 기재! 자기참조를 위해 
+		char name[10];
+		int age;
+		float salary; //field
+	} person; //personal_info라는 구조를 가진 변수(~객체) person 생성
+	struct personal_info person1; //이렇게 personal_info라는 구조를 가진 또다른 구조체 생성 가능
+	
+	typedef struct personal_info1 {
+		char name[10];
+		int age;
+		float salary;
+	} person_type; // 여기서 person은 변수이름이 아님!! typedef B A > B를 A라 정의함 이라는 문법으로 인해 위의 구조체를 의미함
+	person_type p; // p는 personal_info1의 구조를 가진 변수(~객체)
+
+
+
+	omg.name = (char*)malloc(sizeof(char) * 10);
+	homg.name = (char*)malloc(sizeof(char) * 10);
+	
+	strcpy(omg.name, "lee");
+	omg.age = 23;
+	omg.salary = 10000000;
+
+	homg = omg; //이러면 포인터까지 복사되서 원래 할당된 homg의 이름이 저장된 공간은 쓰레기가 되고, omg name을 따라감
+
+	strcpy(homg.name, "hoo");
+	homg.age = 32;
+	homg.salary = 200;
+
+
+	printf("omg name : %s\n", omg.name);
+	printf("omg age : %d\n", omg.age);
+	printf("omg name : %f\n", omg.salary);
+
+	printf("homg name : %s\n", homg.name);
+	printf("homg age : %d\n", homg.age);
+	printf("homg name : %f\n", homg.salary);
+
+}
--- DataStructure/union.c
+++ DataStructure/union.c
@@ -1,43 +1,30 @@
-
-//struct personal_info {
-	//	char name[10];
-	//	int age;
-	//	float salary;
-	//};//구조체 정의
-	//struct personal_info person;//변수 생성
-
-	//typedef struct personal_info1 {
-	//	char name[10];
-	//	int age;
-	//	float salary;
-	//	struct personal_info1* a; //자기참조
-	//} person1;//typedef 'struct' as 'person'
-
-	//strcpy(person.name, "lee");
-	//person.age = 10;
-	//person.salary = 35000;
-
 #include <stdio.h>
-#include <stdbool.h>
+#define FALSE 0
+#define TRUE 1
+
+typedef struct _sexType {
+	enum tagField {female, male, idonknow, youknow, hihi} sex; //enum은 정수로 저장되므로 대부분의 컴파일러에서 4바이트로 정의된다.
+	union _u {
+		int children; //4바이트
+		int beard;//4바이트 >> 가장 큰 값인 4바이트를 children,beard가 공유함
+		//메모리를 공유하기 때문에 멤버별 둘 중 하나만 사용하는게 마땅함. 둘 다 사용할시 메모리 침범 가능성
+	} u;
+} sexType;
+typedef struct _humanBeing {
+	char name[10]; //10바이트
+	int age; //4바이트
+	float salary; // 4바이트
+	int date_of_birth; // 4바이트
+	sexType sexInfo;
+} humanBeing;
+humanBeing person1, person2;
+
+
 int main() {
-	typedef struct _sexType {
-		enum tagField { female, male } sex;
-		union _u {
-			int children;
-			bool beard;
-		} u;
-	} sexType;
-
-	typedef struct _humanBeing {
-		char name[10];
-		int age;
-		float salary;
-		sexType sexInfo;
-	} humanBeing;
-	humanBeing person1, person2;
-
 	person1.sexInfo.sex = male;
-	person1.sexInfo.u.beard = 0;
+	person1.sexInfo.u.beard = FALSE;
+	person1.sexInfo.u.children = 3;
 	person2.sexInfo.sex = female;
 	person2.sexInfo.u.children = 4;
+	printf("size of union : %d", sizeof(person1));
 }
